92cc332a85c8126992dc6cd1df134911faef1ee6
Nicholas Theodore Setijabudiharsa <nicholas@cryptio.co>
2020-06-21 21:22:07 +0200

    add homework submission

---
+++ test.jl
@@ -0,0 +1,17 @@
+function Hello(name)
+    println("Hello ", name)
+end
+
+names = [
+    "Nicholas",
+    "Nicholas1",
+    "Nicholas2",
+    "Nicholas3",
+    "Nicholas4",
+    "Nicholas5",
+    "Nicholas6"
+]
+
+for name in names
+    Hello(name)
+end
